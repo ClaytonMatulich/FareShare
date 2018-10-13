e0cbee174b91f4f7f1ea49b157302f8bf2d5687a
Daniel Tatarkin <danieltatarkin@gmail.com>
2018-10-13 05:27:59 -0700

    Map pannable

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
+
+/**
+ * Boilerplate map initialization code starts below:
+ */
+
+//Step 1: initialize communication with the platform
 var platform = new H.service.Platform({
-    'app_id': '7SDSJvoRdV0v2xZ0BrAV',
-    'app_code': 'WPhUiT2gtYoIBPnzf0VoQg'
-});
-
-// Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
-
-// Instantiate (and display) a map object:
-var landingMap = new H.Map(
-    document.getElementById('landing-map-container'),
-    defaultLayers.normal.map, {
-        zoom: 12,
-        center: {
-            lat: 32.77,
-            lng: -117.07
-        }
-    });
-
-console.log(landingMap)
+    app_id: '7SDSJvoRdV0v2xZ0BrAV',
+    app_code: 'WPhUiT2gtYoIBPnzf0VoQg',
+    useHTTPS: true
+  });
+  var pixelRatio = window.devicePixelRatio || 1;
+  var defaultLayers = platform.createDefaultLayers({
+    tileSize: pixelRatio === 1 ? 256 : 512,
+    ppi: pixelRatio === 1 ? undefined : 320
+  });
+  
+  //Step 2: initialize a map  - not specificing a location will give a whole world view.
+  var map = new H.Map(document.getElementById('landing-map-container'),
+    defaultLayers.normal.map, {pixelRatio: pixelRatio});
+  
+  //Step 3: make the map interactive
+  // MapEvents enables the event system
+  // Behavior implements default interactions for pan/zoom (also on mobile touch environments)
+  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  
+  // Create the default UI components
+  var ui = H.ui.UI.createDefault(map, defaultLayers);
+  
+  // Now use the map as required...
+  setMapViewBounds(map);
---
+++ pannableMap.js
@@ -0,0 +1,4 @@
+function setMapViewBounds(map) {
+    var bbox = new H.geo.Rect(42.3736, -71.0751, 42.3472, -71.0408);
+    map.setViewBounds(bbox);
+}
